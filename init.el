7241e50850300a1ccc9e480ec2ed68f030a6d3e7
Philip Arvidsson <philip@philiparvidsson.com>
2017-12-30 07:47:07 +0100

    tweaks

--- init.el
+++ init.el
@@ -3,6 +3,27 @@
 ;; Author: Philip Arvidsson <hello@philiparvidsson.com>
 ;; URL: https://github.com/philiparvidsson/My-Emacs-Config
 
+;;; License:
+
+;;
+;; Copyright 2018 Philip Arvidsson
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+;; associated documentation files (the "Software"), to deal in the Software without restriction,
+;; including without limitation the rights to use, copy, modify, merge, publish, distribute,
+;; sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in all copies or
+;; substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+;; NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+;; DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+;;
+
 ;;; Code:
 
 ;;;;------------------------------------
@@ -210,9 +231,6 @@
 ;; Iterate backwards through the kill ring with C-M-y.
 (global-set-key (kbd "C-M-y") (lambda () (interactive) (yank-pop -1)))
 
-;; Compile easily.
-(global-set-key (kbd "C-S-b") 'recompile)
-
 ;;;;------------------------------------
 ;;;; 6. Package setup
 ;;;;------------------------------------
@@ -242,7 +260,7 @@
 (require 'multiple-cursors)
 
 (global-set-key (kbd "M-<down>") 'mc/mark-next-like-this)
-(global-set-key (kbd "M-<up>")   'mc/mark-next-like-this-symbol)
+(global-set-key (kbd "M-<up>"  ) 'mc/mark-next-like-this-symbol)
 
 ;;; projectile
 (require 'projectile)
