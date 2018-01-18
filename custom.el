c1933c0e099e3fc71ea4500513dfb1e42018529d
Philip Arvidsson <philip@philiparvidsson.com>
2018-01-18 18:05:45 +0100

    tweaks

--- custom.el
+++ custom.el
@@ -1,15 +1,1 @@
 (ignore)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (company-jedi web-mode spacemacs-theme projectile omnisharp multiple-cursors magit lua-mode js2-mode groovy-mode glsl-mode diminish company))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
